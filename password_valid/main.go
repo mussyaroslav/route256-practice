8245bfc0983f838e00e12c49cb3929f7d9377356
Yaroslav Muss <mussyaroslav@gmail.com>
2024-05-31 20:46:17 +0500

    Сделал задачу на валидность паролей из контеста 05.2023!

--- password_valid/main.go
+++ password_valid/main.go
@@ -11,13 +11,16 @@ func main() {
 	fmt.Scan(&size)
 	fmt.Scan(&str)
 	arr := make([]string, size)
+
 	for i := 0; i < size; i++ {
 		fmt.Scan(&arr[i])
 	}
+
 	runes := []rune(str)
 	sort.Slice(runes, func(i, j int) bool {
 		return runes[i] < runes[j]
 	})
+
 	for i, value := range arr {
 		runes1 := []rune(value)
 		sort.Slice(runes1, func(i, j int) bool {
@@ -25,6 +28,7 @@ func main() {
 		})
 		arr[i] = string(runes1)
 	}
+
 	for _, value := range arr {
 		if value == string(runes) {
 			fmt.Println("YES")
